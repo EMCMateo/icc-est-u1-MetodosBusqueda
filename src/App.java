23dbe178b38df3bc8c0b19f49248e8f222bd6be6
EMCMateo <mateo3456moli@gmail.com>
2025-04-15 07:26:05 -0500

    Creacion de Diagrama UML en el AVAC

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import controllers.MetodosBusqueda;
+public class App {
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+
+        MetodosBusqueda metodos = new MetodosBusqueda();
+
+        
+    }
+}
---
+++ src/controllers/MetodosBusqueda.java
@@ -0,0 +1,8 @@
+package controllers;
+import views.ShowConsole;
+public class MetodosBusqueda{
+
+    public int [] busquedaLineal = new int[10];
+
+    ShowConsole show = new ShowConsole();
+}
