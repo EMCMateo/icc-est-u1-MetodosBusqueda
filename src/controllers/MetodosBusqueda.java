9f96cf8955c6a315e6cb9f86d3e84f5dee662722
EMCMateo <mateo3456moli@gmail.com>
2025-04-15 21:38:30 -0500

    Busqueda por codigo 1

--- src/controllers/MetodosBusqueda.java
+++ src/controllers/MetodosBusqueda.java
@@ -1,30 +1,31 @@
 package controllers;
 import views.ShowConsole;
 import models.Persona;
-public class MetodosBusqueda{
+
+public class MetodosBusqueda {
     private ShowConsole show;
     private Persona[] personas = new Persona[10];
+
     public MetodosBusqueda() {
         show = new ShowConsole();
     }
+
     public MetodosBusqueda(Persona[] persons) {
         show = new ShowConsole();
-        this.personas = personas;
+        this.personas = persons; // Corrección: Asignar correctamente el parámetro
         showPersonByCoode();
     }
 
-    public int FindByPerson(int codigo){
+    public int FindByPerson(int codigo) {
         for (int i = 0; i < personas.length; i++) {
-                if (personas[i].getCodigo() == codigo) {
-                    System.out.println("El elemento " + codigo + " se encuentra en la posicion " + i);
-                    return i;
-                }else {
-               
-                }
-         
-    }return -1;
-}
-    
+            if (personas[i] != null && personas[i].getCodigo() == codigo) { // Validación de nulos
+                System.out.println("El elemento " + codigo + " se encuentra en la posición " + i);
+                return i;
+            }
+        }
+        return -1; // Si no se encuentra, devolver -1
+    }
+
     public void showPersonByCoode() {
         int codeToFind = show.getCodigo();
         int indexPersona = FindByPerson(codeToFind);
@@ -32,10 +33,7 @@ public class MetodosBusqueda{
             show.showMessage("Persona encontrada");
             show.showMessage(personas[indexPersona].toString());
         } else {
-            show.showMessage("Persona no encontrada con el codigo: " + codeToFind);
+            show.showMessage("Persona no encontrada con el código: " + codeToFind);
         }
     }
-    
-
-    
 }
